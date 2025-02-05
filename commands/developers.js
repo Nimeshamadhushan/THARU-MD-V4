ae7527ca4759f3b66d7520477f248d84573dc3d3
Nimeshamadhushan <89959527+Nimeshamadhushan@users.noreply.github.com>
2025-02-06 05:17:48 +0530

    THARU MD 😇❤️

---
+++ commands/developers.js
@@ -0,0 +1,101 @@
+const {fetchJson,cmd, tlang,sleep } = require('../lib')
+
+//---------------------------------------------------------------------------
+
+cmd({
+
+            pattern: "Developer",
+
+            category: "tools",
+
+            desc: "Developers info",
+
+            use: ' ',
+
+            filename: __filename,
+
+        },
+
+        async(Void,citel, text) => {
+
+citel.reply("```Sending developer information```")
+
+await sleep(1000)
+
+citel.reply("```NIMA```")
+
+
+await sleep(1000)
+
+citel.reply("```NIMA```")
+
+await sleep(1000)
+
+citel.reply("```NIMA```")
+
+await sleep(1000)
+
+citel.reply("```NIMA```")
+
+await sleep(1000)
+
+citel.reply("```NIMA```")
+
+await sleep(1000)
+
+citel.reply("```OTHER BOTS FROM DEVELOPER```")
+
+await sleep(1000)
+
+citel.reply("```NIMA:-\n https://github.com/Nimeshamadhushan/THARU-MD-V4```")
+
+await sleep(1000)
+
+citel.reply("```NIMA:- https://github.com/Nimeshamadhushan/THARU-MD-V4```")
+
+await sleep(1000)
+
+citel.reply("```NIMA:-\n https://github.com/Nimeshamadhushan/THARU-MD-V4```")
+
+await sleep(1000)
+
+citel.reply("```NIMA:-\n https://github.com/Nimeshamadhushan/THARU-MD-V4```")
+
+await sleep(1000)
+
+citel.reply("```https://github.com/Nimeshamadhushan/THARU-MD-V4```")
+
+await sleep(1000)
+
+citel.reply("```https://github.com/Nimeshamadhushan/THARU-MD-V4")
+
+await sleep(1000)
+
+citel.reply("```https://github.com/Nimeshamadhushan/THARU-MD-V4```")
+
+await sleep(1000)
+
+citel.reply("```https://github.com/Nimeshamadhushan/THARU-MD-V4```")
+
+await sleep(1000)
+
+citel.reply("``` https://github.com/Nimeshamadhushan/THARU-MD-V4 ```")
+
+await sleep(1000)
+
+citel.reply("```https://github.com/Nimeshamadhushan/THARU-MD-V4```")
+
+await sleep(1000)
+
+citel.reply("```NIMA CODER ```")
+
+await sleep(1000) 
+
+citel.reply("```ʙʏ 94789549564```")
+
+
+            return citel.reply('*THOSE ARE MY DEVELOPERS *');
+
+        }
+
+    )
